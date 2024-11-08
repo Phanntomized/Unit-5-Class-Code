8d43c19cca6abe996f8401b0d5101ce4e2736443
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-11-08 17:41:35 +0000

    BUST

--- lesson_2.py
+++ lesson_2.py
@@ -7,7 +7,7 @@ def rainy():
 def snowy():
     print("On a snowy day, boots are appropriate footwear.")
 
-def main():
+def input():
     weather = input("What is the weather? (sunny, rainy, snowy) ")
     if weather == "sunny":
         sunny()
@@ -19,5 +19,29 @@ def main():
         print("Invalid option.")
         main()
 
+
+
+def main(name="John",color="yellow"):
+    print(f"{name.title()} the fierce, {color.lower()} eyed warrior")
+
+def blackjack():
+    card = 0
+    eleven = False
+    num = 0
+    import random
+    for i in range(3):
+        num = random.randint(1,11)
+        if num == 11:
+            eleven = True
+        card = card + num
+    if card <= 21:
+        print(str(card))
+    elif card > 21 and eleven:
+        print(str(card-10))
+    elif card > 21 and not eleven:
+        print("BUST")
+    else:
+        print("ERROR")
+
 if __name__ == "__main__":
-    main()
+    blackjack()
